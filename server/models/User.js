7ebad49419f7ecd73262e1e7cceff9949a417d4d
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2025-08-31 08:15:44 +0530

    d

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     _id: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
-    Image: { type: String, required: true },
+    image: { type: String, required: true },
     role: { type: String, enum: ["user", "agencyOwner"], default: "user" },
     recentSearchedCity: [{ type: String }],
 }, { timestamps: true })
